c683402eaa6814f1af114a1958eb7d3ddbc31b5e
whatevermane <zmatt33@gmail.com>
2019-06-05 15:34:35 -0500

    edited card component

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,20 +1,16 @@
 import React from "react";
-//import "./style.css";
 
-function Card(simpsons) {
+export default function Card(props) {
+  // console.log(props)
   return (
-    <div className="card">
-      <div className="img-container">
-        <img className="img-fluid" alt={simpsons.name} src={simpsons.image} />
-      </div>
-      <span
-        onClick={() => simpsons.removeSimpson(simpsons.id)}
-        className="remove"
-      >
-        {" "}
-      </span>
+    <div className="card btn" onClick={props.handleClick}>
+      <img
+        id={`simpson-card${props.id}`}
+        key={props.id}
+        src={props.img}
+        alt={props.name}
+        className="img-fluid"
+      />
     </div>
   );
 }
-
-export default Card;
