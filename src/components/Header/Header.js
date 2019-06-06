85b133f6fb66fd75a7a7775461e48aac1787f6b5
whatevermane <zmatt33@gmail.com>
2019-06-06 16:31:23 -0500

    Added some more style

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,11 @@
 import React from "react";
-import "./style.css";
+//import "./style.css";
 
 export default function Header(props) {
   return (
     <header>
-      <nav className="navbar purple darken-3 text-success sticky-top">
-        <ul className="container text-center">
+      <nav className="navbar blue darken-3 text-success sticky-top">
+        <ul className="container">
           <li className="nav-link">{props.message}</li>
           <li className="nav-link">
             Score: {props.score} | Top Score: {props.topScore}
@@ -14,7 +14,13 @@ export default function Header(props) {
       </nav>
 
       <div className="jumbotron-fluid text-center p-2 clearfix">
-        <h1 className="display-3">Simpsons Memory Game</h1>
+        <img
+          src={
+            "https://images.launchbox-app.com/60cc0ee9-c961-4f00-b597-99f6429ecf6f.png"
+          }
+          alt="logo"
+          className="logo img-responsive"
+        />
         <p className="lead">
           Click any character to begin. Try to use your memory and avoid
           clicking the same image twice.
